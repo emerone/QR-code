45c36ae10702dd8894ace1de969d7f4b3985828d
Emmanuel <emmanuel.lebaramo@gmail.com>
2022-10-26 21:53:40 +0200

    write part

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -20,11 +20,34 @@ const btnW = document.querySelector('div.write button')
 const imgW = document.querySelector('div.write img')
 const formW = document.querySelector('div.write')
 
+
 const writeF = (e) => {
-    if (e.key == "Enter" || e.target.nodeName == 'BUTTON'){
+
+    // API used from: goqr.me
+    data = inpW.value.replace(/&/g, '')
+    if (data != '')     { formW.classList.remove("not-valid") }
+    if (data == '')     { formW.classList.add("not-valid")}
+    if (data != false) 
+    if (data == false) 
+
+    console.log(data)
+    function url() { return `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}` }
+
+    function imgShow() {
         formW.classList.remove('not-active')
+        imgW.src = url()
+        console.log(url())
+    }
+
+    if (e.target.nodeName == 'BUTTON' && data != false){
+        imgShow()
+    }
+
+    if (e.key == "Enter" && data != false){
+        imgShow()
     }
 }
 
 inpW.addEventListener('keydown', writeF)
+inpW.addEventListener('input', writeF)
 btnW.addEventListener('click', writeF)
